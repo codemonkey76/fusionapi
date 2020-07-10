1a05ba149e6782247a9d09276f5cf5616ebfff76
Shane Poppleton <shane@laphasg.com.au>
2020-07-10 13:40:22 +1000

    Added default route

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function() {
+    return "<html><head><title>Fusion API</title></head><body>You have reached the fuion API</body></html>";
+});
