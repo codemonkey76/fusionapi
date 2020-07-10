fb1a4ee032ebb91b41dcfc05e96f3e371a9b5b3d
Shane Poppleton <shane@laphasg.com.au>
2020-07-10 13:40:45 +1000

    Added default route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {
-    return "<html><head><title>Fusion API</title></head><body>You have reached the fuion API</body></html>";
+    return "<html><head><title>Fusion API</title></head><body>You have reached the fusion API</body></html>";
 });
