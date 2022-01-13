a0a5e6528970162c2d2fcfc5fc7dd939eb9a9589
Shane Poppleton <shane@bjja.com.au>
2022-01-13 12:23:20 +1000

    Added fpm and fix folder permissions

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ sudo apt install -y ca-certificates apt-transport-https software-properties-comm
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
 wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
 sudo apt update
-sudo apt install -y php8.0 php-xml php-curl php-sqlite3 php-pgsql
+sudo apt install -y php8.0 php-xml php-curl php-sqlite3 php-pgsql php8.0-fpm
 
 #Install composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -22,13 +22,15 @@ composer install
 cp .env.example .env
 php artisan key:generate
 touch database/database.sqlite
+sudo chown -R www-data:www-data ./public
+sudo chown -R www-data:www-data ./storage
 
 # Alter the .env to grab the local FusionPBX database password
 sed -i "s/DB2_PASSWORD=$/DB2_PASSWORD=$(cat /etc/fusionpbx/config.php | grep db_password | awk -F\' '{print $2}')/g" .env
 
 # alter the nginx config so that the servername is reflected
 sed -i "s/server_name ;/server_name $(hostname -f);/g" ~/fusionapi/nginx/fusionapi
-
+sed -i "s/root ;/root $(pwd)\/public;/g" ~/fusionapi/nginx/fusionapi
 sudo ln -s ~/fusionapi/nginx/fusionapi /etc/nginx/sites-enabled/fusion-api
 
 sudo /etc/init.d/nginx reload
