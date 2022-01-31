968ced44815622bdc3de4584877ae1067df1a0f0
Shane Poppleton <shane@bjja.com.au>
2022-01-31 12:52:11 +1000

    Fixed command in active call controller

--- app/Http/Controllers/ActiveCallController.php
+++ app/Http/Controllers/ActiveCallController.php
@@ -8,7 +8,7 @@ class ActiveCallController extends Controller
 {
     public function index()
     {
-        $p = new Process(['fs_cli', '-x "show calls as json"']);
+        $p = new Process(['fs_cli', '-x show calls as json']);
         $p->run();
 
         return response()->json($p->getOutput());
