91ba8676fb56e2dce67bd9416c461ed3171e9c84
Shane Poppleton <shane@bjja.com.au>
2022-02-07 08:10:55 +1000

    Fix regex

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~
 
-DISTRO=$(lsb_release -i | cut -d: -f2 | sed 's/<TAB>//')
+DISTRO=$(lsb_release -i | cut -d: -f2 | sed 's/^\s//g')
 
 
 sudo apt install -y software-properties-common
